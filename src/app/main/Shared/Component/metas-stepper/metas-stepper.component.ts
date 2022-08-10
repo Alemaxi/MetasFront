e8c9c1aff6c35dc242fc971d85593362dbd849d9
alexandre <alexandre.santos@metatron.com.br>
2022-08-10 14:25:14 -0300

    Feature (general commit): Third commit.
    General items inserted to the project.
    
    Feature.

--- src/app/main/Shared/Component/metas-stepper/metas-stepper.component.ts
+++ src/app/main/Shared/Component/metas-stepper/metas-stepper.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-metas-stepper',
@@ -9,7 +9,12 @@ export class MetasStepperComponent implements OnInit {
 
   constructor() { }
 
+  @Input() status:number = 0;
+
   ngOnInit(): void {
   }
 
+  biggerOrEqualTo(num: number): boolean {
+    return !(num <= this.status);
+  }
 }
