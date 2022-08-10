85535162fa3d797f7d4547adfb3ca5779679d206
alexandre <alexandre.santos@metatron.com.br>
2022-08-10 18:10:35 -0300

    Feature (Home-Table): Features added
    Some more changes were made to Home-Table component.
    2 files were modified and 1 file were created.
    
    Feature: 2 modified files and 1 file was created.

--- src/app/main/Shared/Component/home-table/home-table.component.ts
+++ src/app/main/Shared/Component/home-table/home-table.component.ts
@@ -1,5 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+import { simuladorEntity } from '../../Entities/simulador-entity';
+
+export const listSimulator: simuladorEntity[] = [
+  {
+    indicador: 'nomeIndicador',
+    unidadeMedida: '%',
+    frequencia:10,
+    desafio:5,
+    minimo:3,
+    planejado:4,
+    resultado:5,
+    simulacao:4.5,
+    peso:5,
+  },
+  {
+    indicador: 'nomeIndicador',
+    unidadeMedida: '%',
+    frequencia:10,
+    desafio:5,
+    minimo:3,
+    planejado:4,
+    resultado:5,
+    simulacao:4.5,
+    peso:5,
+  },
+  {
+    indicador: 'nomeIndicador',
+    unidadeMedida: '%',
+    frequencia:10,
+    desafio:5,
+    minimo:3,
+    planejado:4,
+    resultado:5,
+    simulacao:4.5,
+    peso:5,
+  }
+]
+
 @Component({
   selector: 'app-home-table',
   templateUrl: './home-table.component.html',
@@ -7,9 +45,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeTableComponent implements OnInit {
 
-  constructor() { }
+  itemList: simuladorEntity[] = [];
+
+  constructor() { 
+    this.itemList = listSimulator;
+  }
 
   ngOnInit(): void {
+  }
+
+  GetFrequenciaTotal():number{
+    let result: number = 0;
+
+    this.itemList.forEach( (x:simuladorEntity) =>{
+      result += x.frequencia;
+    })
+
+    return result;
   }
 
 }
